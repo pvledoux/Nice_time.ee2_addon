2489f5017b20ed173b19a9186224c231c11451e9
Arthur Vincent <arthurvincent.simon@gmail.com>
2013-09-02 13:54:18 +0800

    Added language files

---
+++ system/expressionengine/third_party/nice_time/language/english/nice_time_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang = array(
+	'time_ago_now' => 'now',
+	'time_ago_from_now' => '%d from now',
+	'time_ago_ago' => '%d ago',
+	'time_ago_second' => '% second ago',
+	'time_ago_seconds' => '% seconds ago',
+	'time_ago_minute' => '% minute ago',
+	'time_ago_minutes' => '% minutes ago',
+	'time_ago_hour' => '% hour ago',
+	'time_ago_hours' => '% hours ago',
+	'time_ago_day' => '% day ago',
+	'time_ago_days' => '% days ago',
+	'time_ago_week' => '% week ago',
+	'time_ago_weeks' => '% weeks ago',
+	
+	'format_full_date' => '%d-%m-%Y %H:%i'
+);
---
+++ system/expressionengine/third_party/nice_time/language/xxxxx/nice_time_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang = array(
+	'time_ago_now' => 'xxxx',
+	'time_ago_from_now' => '%d xxxx xxxx',
+	'time_ago_ago' => '%d xxxx',
+	'time_ago_second' => '% xxxx xxxx',
+	'time_ago_seconds' => '% xxxx xxxx',
+	'time_ago_minute' => '% xxxx xxxx',
+	'time_ago_minutes' => '% xxxx xxxx',
+	'time_ago_hour' => '% xxxx xxxx',
+	'time_ago_hours' => '% xxxx xxxx',
+	'time_ago_day' => '% xxxx xxxx',
+	'time_ago_days' => '% xxxx xxxx',
+	'time_ago_week' => '% xxxx xxxx',
+	'time_ago_weeks' => '% xxxx xxxx',
+	
+	'format_full_date' => '%d-%m-%Y %H:%i'
+);
